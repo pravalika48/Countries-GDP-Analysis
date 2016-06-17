d8e3467c71eba513703dbde134cf2743de81fdcf
sharanyadaram <sharanyadaram@gmail.com>
2016-06-17 22:10:28 +0530

    Add files via upload

---
+++ IHvsIU.R
@@ -0,0 +1,2 @@
+
+barplot(as.integer(factbookclass$Internet.users), width = 5, names.arg = factbookclass$Internet.hosts., col = "red", main = "Internethosts Vs Internetusers", xlab="InternetHost", ylab="InternetUsers", las=1)
