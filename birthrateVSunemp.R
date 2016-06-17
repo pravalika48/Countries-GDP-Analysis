a1b5e4b76c2705314433bfefd943ef02af218896
pravalika48 <pravalika.anumula4@gmail.com>
2016-06-17 09:50:10 +0530

    Add files via upload

---
+++ birthrateVSunemp.R
@@ -0,0 +1,2 @@
+
+barplot(as.integer(factbookclass$Unemployment.rate...), width = 5, names.arg = factbookclass$Birth.rate.births.1000.population., main = "birthrate Vs unemployment", xlab="BirthRate", ylab="Unemployment", las=1)
