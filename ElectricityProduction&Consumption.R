2cd5c21e8506f3344b65cb9a57543a9cf03ba17c
pravalika48 <pravalika.anumula4@gmail.com>
2016-06-17 09:59:59 +0530

    Add files via upload

---
+++ ElectricityProduction&Consumption.R
@@ -0,0 +1,8 @@
+
+
+
+
+factbookclass[order(factbookclass$Electricity...production.kWh., decreasing = TRUE)[1:5],]
+factbookclass[order(factbookclass$Electricity...production.kWh., decreasing = FALSE)[1:5],]
+factbookclass[order(factbookclass$Electricity...consumption.kWh., decreasing = TRUE)[1:5],]
+factbookclass[order(factbookclass$Electricity...consumption.kWh., decreasing = FALSE)[1:5],]
